e073277ff4bee88aa276422688f5320604cadf32
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2023-03-04 09:24:49 +0900

    django_winter 4week - 배포 전

---
+++ blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import *
+from markdownx.admin import MarkdownxModelAdmin
+# Register your models here.
+
+admin.site.register(Post, MarkdownxModelAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('name', )}
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('name', )}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment)
+
---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.category_page), #category/뒤의 문자열을 category_page 함수의 매개변수 slug의 인자로 넘긴다.
+    path('tag/<str:slug>/', views.tag_page),
+    path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('<int:pk>/new_comment/', views.new_comment),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/',views.delete_comment),
+    path('search/<str:q>/', views.PostSearch.as_view()),
+]
